c9268db4575689183ef516dc3ef0c35cf541352c
Robert Schoene <robert.schoene@tu-dresden.de>
2016-01-07 13:07:01 +0100

    changes: continues, if it fails for one CPU, more options, die is now called node, option is now called item ...

--- library/examples/x86a_read.c
+++ library/examples/x86a_read.c
@@ -1,3 +1,52 @@
+/*************************************/
+/**
+ * @file x86a_read
+ * @brief Example application to read the values of all available knobs on the system. 
+ * 
+ * @synopsis x86a_read [ x86a_read-ARGS ...]
+ * Invoking the x86a_read tool prints a list of knobs:
+ * @code
+ * Item 0: Intel_Clock_Modulation_Value
+ * ----------------
+ * Item 1: Intel_DCU_Prefetch_Disable
+ * ----------------
+ * Item 2: Intel_Enhanced_SpeedStep
+ * ----------------
+ * Item 3: Intel_Target_PState
+ * [...]
+ * @endcode
+ * Add \-\-verbose to get a short description of each knob. <br> 
+ * The tool also prints a the current settings of each knob as CSV.
+ * 
+ * Options and Usage:
+ * @code
+ * Usage: x86a_read [ x86a_read-ARGS ...]
+ * x86a_read-ARGS:
+ * 	 -h --help: print this help
+ * 	 -H --hex: print readings in hexadecimal form
+ * 	 -n --node: print node options instead of CPU options
+ * 	 -c --cpu: Read item(s) from this CPU (default=all)
+ * 		If -n is set, read item(s) from this node (default=all).
+ * 	 -i --item: Read this item (default=all)
+ * 	 -v --verbose: print more information
+ * @endcode
+ * Details:
+ * The term "node" refers to a node in the Linux context, which is a NUMA node (e.g., a physical processor).
+ * Examples:
+ * 
+ * @code
+ * x86a_read -c 0 # prints all items and the readings for CPU 0
+ * x86a_read -n 0 -c 0 # prints all items and the readings for node 0
+ * x86a_read -n 0 -c 0 -X # prints all items and the readings for node 0 in hexadecimal
+ * x86a_read -i Intel_Clock_Modulation_Value -c 0 -X # prints the item Intel_Clock_Modulation_Value and its reading for CPU 0 in hexadecimal
+ * @endcode
+ *
+ * Use the tool @ref x86a_write.c "x86a_write" to change a setting.
+ *
+ * @TODO merge code from x86a_read.c and x86a_write.c
+ * @author Robert Schoene robert.schoene@tu-dresden.de 
+ */
+
 #include <stdio.h>
 #include "x86_adapt.h"
 #include <string.h>
@@ -7,60 +56,32 @@
 #include <stdlib.h>
 #include <libgen.h>
 
-/*************************************/
-/**
- * @file x86a_read.c
- * @brief Example application to read the values of all available knobs on the system. 
- * 
- * Please see the help text (-h) for details on how to use it. 
- * 
- * Invoking the x86a_read tool prints a list of knobs:
- * @code
- * Item 0: Intel_Clock_Modulation_Extended_Value
- * ----------------
- * Item 1: Intel_DCU_Prefetch_Disable
- * ----------------
- * Item 2: Intel_Enhanced_SpeedStep
- * ----------------
- * Item 3: Intel_Target_PState
- * ----------------
- * Item 4: Intel_Extended_Clock_Modulation_Extended
- * [...]
- * @endcode
- * Add \-\-verbose to get a short description of each knob. <br> 
- * The tool also prints a the current settings of each knob as CSV.
- * 
- * Use the tool @ref x86a_write.c "x86a_write" to change a setting.
- *
- * @author Robert Schoene robert.schoene@tu-dresden.de 
- */
-
 
 static void print_help(char ** argv)
 {
   char *path = strdup(argv[0]);
   char *base = basename(path);
   fprintf(stderr, "\n");
-  fprintf(stderr, "Usage: %s [ %s-ARGS ...] \"COMMAND [ ARGS ...]\"\n", base, base);
+  fprintf(stderr, "Usage: %s [ %s-ARGS ...]\n", base, base);
   fprintf(stderr, "\n");
   fprintf(stderr, "%s-ARGS:\n", base);
   fprintf(stderr, "\t -h --help: print this help\n");
-  fprintf(stderr, "\t -H --hex: print results in hexadecimal form\n");
-  fprintf(stderr, "\t -d --die: print die options instead of CPU options\n");
-  fprintf(stderr, "\t -c --cpu: Use this CPU (default=all)\n"
-                                "If -d is set, use the die with this nr.\n");
+  fprintf(stderr, "\t -H --hex: print readings in hexadecimal form\n");
+  fprintf(stderr, "\t -n --node: print node options instead of CPU options\n");
+  fprintf(stderr, "\t -c --cpu: Read item(s) from this CPU (default=all)\n"
+                     "\t\tIf -n is set, read item(s) from this node (default=all).\n");
+  fprintf(stderr, "\t -i --item: Read this item (default=all)\n");
   fprintf(stderr, "\t -v --verbose: print more information\n");
   fprintf(stderr, "\n");
-  fprintf(stderr, "This program reads all available CPU knobs from x86_adapt.\n");
+  fprintf(stderr, "This program reads all available CPU/node knobs from x86_adapt.\n");
   free(path);
 }
 
-static void print_cis(x86_adapt_device_type type, int verbose)
+static void print_cis(x86_adapt_device_type type, int verbose, int item_min, int item_max)
 {
   struct x86_adapt_configuration_item item;
-  int nr_items = x86_adapt_get_number_cis(type);
   int ci_nr = 0;
-  for ( ci_nr = 0 ; ci_nr < nr_items ; ci_nr++ )
+  for ( ci_nr = item_min ; ci_nr < item_max ; ci_nr++ )
   {
     if (x86_adapt_get_ci_definition(type,ci_nr,&item))
     {
@@ -74,51 +95,49 @@ static void print_cis(x86_adapt_device_type type, int verbose)
   }
 }
 
-static void print_header(x86_adapt_device_type type)
+static void print_header(x86_adapt_device_type type, int item_min, int item_max)
 {
-  int nr_items = x86_adapt_get_number_cis(type);
   int ci_nr = 0;
 
   switch (type)
   {
     case X86_ADAPT_CPU:
-      fprintf(stdout,"CPU;");
+      fprintf(stdout,"CPU");
       break;
     case X86_ADAPT_DIE:
-      fprintf(stdout,"Node;");
+      fprintf(stdout,"Node");
       break;
     default:
       fprintf(stderr, "WARN: unknown type!\n");
       break;
   }
-  for ( ci_nr = 0 ; ci_nr < nr_items ; ci_nr++ )
+  for ( ci_nr = item_min ; ci_nr < item_max ; ci_nr++ )
   {
-    fprintf(stdout,"Item %d;",ci_nr);
+    fprintf(stdout,",Item %d",ci_nr);
   }
   fprintf(stdout,"\n");
 }
 
-static void print_cpu(x86_adapt_device_type type, int cpu, int print_hex)
+static void print_cpu(x86_adapt_device_type type, int cpu, int print_hex, int item_min, int item_max)
 {
-  int nr_items = x86_adapt_get_number_cis(type);
   int fd = x86_adapt_get_device_ro(type, cpu);
   uint64_t result;
   int ret;
   int ci_nr = 0;
-  fprintf(stdout,"%d;",cpu);
+  fprintf(stdout,"%d",cpu);
   if ( fd > 0 )
   {
-    for ( ci_nr = 0 ; ci_nr < nr_items ; ci_nr++ )
+    for ( ci_nr = item_min ; ci_nr < item_max ; ci_nr++ )
     {
       if ((ret = x86_adapt_get_setting(fd,ci_nr,&result)) != 8)
       {
-        fprintf(stderr,"Could not read definitions for cpu/die %d\n",cpu);
-        exit(1);
+        fprintf(stderr,"Could not read item %d for cpu/die %d\n",ci_nr,cpu);
+        return;
       }
       if (print_hex)
-        fprintf(stdout,"%"PRIx64";",result);
+        fprintf(stdout,",%"PRIx64,result);
       else
-        fprintf(stdout,"%"PRIu64";",result);
+        fprintf(stdout,",%"PRIu64,result);
     }
   } else
   {
@@ -135,8 +154,10 @@ int main(int argc, char ** argv)
   int hex=0;
   char c;
   int verbose=0;
+  char * knob=NULL;
+  int item_min=-1, item_max=-1, ci_nr;
+  struct x86_adapt_configuration_item item;
   x86_adapt_device_type type = X86_ADAPT_CPU;
-
 
   while (1)
   {
@@ -144,14 +165,15 @@ int main(int argc, char ** argv)
       {
           {"help", no_argument, 0, 'h'},
           {"hex", no_argument, 0, 'H'},
-          {"die", no_argument, 0, 'd'},
+          {"node", no_argument, 0, 'n'},
           {"cpu", required_argument, 0, 'c'},
+          {"item", required_argument, 0, 'i'},
           {"verbose", no_argument, 0, 'v'},
           {0,0,0,0}
       };
     int option_index = 0;
 
-    c = getopt_long(argc, argv, "hHdc:v", long_options, &option_index);
+    c = getopt_long(argc, argv, "c:i:hHnv", long_options, &option_index);
     if (c == -1)
       break;
     switch (c) {
@@ -161,11 +183,14 @@ int main(int argc, char ** argv)
     case 'H':
       hex = 1;
       break;
-    case 'd':
+    case 'n':
       type = X86_ADAPT_DIE;
       break;
     case 'c':
       cpu = atoll(optarg);
+      break;
+    case 'i':
+      knob=strdup(optarg);
       break;
     case 'v':
       verbose = 1;
@@ -180,18 +205,50 @@ int main(int argc, char ** argv)
     fprintf(stderr,"Could not initialize x86_adapt library");
   }
 
-  print_cis(type, verbose);
-  print_header(type);
 
+  if (knob != NULL)
+  {
+    int nr_items = x86_adapt_get_number_cis(type);
+    /* get item index */
+    for ( ci_nr = 0 ; ci_nr < nr_items ; ci_nr++ )
+    {
+      if (x86_adapt_get_ci_definition(type,ci_nr,&item))
+      {
+        fprintf(stderr,"Could not read item definition\n");
+        exit(1);
+      }
+      if (strcmp(item.name,knob)==0)
+      {
+        item_min = ci_nr;
+        item_max = ci_nr+1;
+        break;
+      }
+    }
+    /* not found? */
+    if ( item_min == -1 )
+    {
+      fprintf(stderr,"The option %s is not available. Available options are listed now:\n",knob);
+      print_cis(type, verbose,0,x86_adapt_get_number_cis(type));
+      exit(1);
+    }
+  }
+  else
+  {
+    item_min = 0;
+    item_max = x86_adapt_get_number_cis(type);
+  }
+
+  print_cis( type, verbose, item_min, item_max );
+  print_header( type, item_min, item_max );
   if ( cpu == -1 )
   {
     for ( cpu = 0 ; cpu < x86_adapt_get_nr_avaible_devices(type) ; cpu++ )
     {
-      print_cpu(type, cpu, hex);
+      print_cpu( type, cpu, hex, item_min, item_max );
     }
   } else
   {
-    print_cpu(type, cpu, hex);
+    print_cpu( type, cpu, hex, item_min, item_max );
   }
   
 
--- library/examples/x86a_write.c
+++ library/examples/x86a_write.c
@@ -1,3 +1,20 @@
+/*************************************/
+/**
+ * @file x86a_write.c
+ * @brief Example application to set the value of a x86_adapt knob on the system. 
+ * 
+ * Please see the help text (-h) for details on how to use it. 
+ * The most common way is to specify the option or knob via -i and the value to be set via -V:
+ * @code 
+ * ./x86a_write -i Intel_Package_CState_Limit -V 3
+ * @endcode
+ *
+ * Use the tool @ref x86a_read.c "x86a_read" to query all available knobs and their values on the system.
+ * 
+ * @TODO merge code from x86a_read.c and x86a_write.c
+ * @author Robert Schoene robert.schoene@tu-dresden.de 
+ *************************************/ 
+
 #include <stdio.h>
 #include "x86_adapt.h"
 #include <string.h>
@@ -7,41 +24,24 @@
 #include <stdlib.h>
 #include <libgen.h>
 
-/*************************************/
-/**
- * @file x86a_write.c
- * @brief Example application to set the value of a x86_adapt knob on the system. 
- * 
- * Please see the help text (-h) for details on how to use it. 
- * The most common way is to specify the option or knob via -o and the value to be set via -V:
- * @code 
- * ./x86a_write -o Intel_Package_CState_Limit -V 3
- * @endcode
- *
- * Use the tool @ref x86a_read.c "x86a_read" to query all available knobs and their values on the system.
- * 
- * @author Robert Schoene robert.schoene@tu-dresden.de 
- *************************************/ 
-
-
 static void print_help(char ** argv)
 {
   char *path = strdup(argv[0]);
   char *base = basename(path);
   fprintf(stderr, "\n");
-  fprintf(stderr, "Usage: %s [ %s-ARGS ...] \"COMMAND [ ARGS ...]\"\n", base, base);
+  fprintf(stderr, "Usage: %s [ %s-ARGS ...] -i <item name> -V <value>\n", base, base);
   fprintf(stderr, "\n");
   fprintf(stderr, "%s-ARGS:\n", base);
   fprintf(stderr, "\t -h --help: print this help\n");
   fprintf(stderr, "\t -H --hex: set in hexadecimal form\n");
-  fprintf(stderr, "\t -d --die: set die options instead of CPU options\n");
-  fprintf(stderr, "\t -c --cpu: Use this CPU (default=all)\n"
-                                "If -d is set, use the die with this nr.\n");
-  fprintf(stderr, "\t -o --option: set this option\n");
+  fprintf(stderr, "\t -n --node: set node options instead of CPU options\n");
+  fprintf(stderr, "\t -c --cpu: Set item of this CPU (default=all)\n"
+                        "\t\tIf -n is set, set the item of this node (default=all).\n");
+  fprintf(stderr, "\t -i --item: set this item\n");
   fprintf(stderr, "\t -V --value: set to this value\n");
   fprintf(stderr, "\t -v --verbose: print more information\n");
   fprintf(stderr, "\n");
-  fprintf(stderr, "This program reads all available CPU knobs from x86_adapt.\n");
+  fprintf(stderr, "This program allows users to write x86_adapt CPU and node knobs.\n");
   free(path);
 }
 
@@ -65,47 +65,23 @@ static void print_cis(x86_adapt_device_type type, int verbose)
   }
 }
 
-static void set_cpu(x86_adapt_device_type type, int cpu, char* knob, uint64_t new_setting,int verbose)
+static void set_cpu(x86_adapt_device_type type, int cpu, int item, uint64_t new_setting,int verbose)
 {
-  struct x86_adapt_configuration_item item;
-  int nr_items = x86_adapt_get_number_cis(type);
   int fd = x86_adapt_get_device(type, cpu);
   int ret;
-  int ci_nr = 0;
   if ( fd > 0 )
   {
-    int ci_found = 0;
-    for ( ci_nr = 0 ; ci_nr < nr_items ; ci_nr++ )
+    if ((ret = x86_adapt_set_setting(fd,item,new_setting)) != 8)
     {
-      if (x86_adapt_get_ci_definition(type,ci_nr,&item))
-      {
-        fprintf(stderr,"Could not read item definition\n");
-        exit(1);
-      }
-      if (strcmp(item.name,knob)==0)
-      {
-        ci_found = 1;
-        if  (verbose)
-          fprintf(stdout,"Set definition %s on cpu/die %d to %"PRIi64"\n",knob,cpu,new_setting);
-        if ((ret = x86_adapt_set_setting(fd,ci_nr,new_setting)) != 8)
-        {
-          fprintf(stderr,"Could not set definitions for cpu/die %d\n",cpu);
-          exit(1);
-        }
-      }
+      fprintf(stderr,"Could not set item for %s %d\n",type==X86_ADAPT_DIE?"node":"CPU",cpu);
+      return;
     }
-    if ( !ci_found )
-    {
-      fprintf(stderr,"The option %s is not available. Available options are listed now:\n",knob);
-      print_cis(type, verbose);
-      exit(1);
-    }
+    x86_adapt_put_device(type, cpu);
   } else
   {
-    fprintf(stderr,"Could not open /dev/x86_adapt/... file for cpu/die %d\n",cpu);
-    exit(-1);
+    fprintf(stderr,"Could not open /dev/x86_adapt/%s/%d file\n",type==X86_ADAPT_DIE?"node":"CPU",cpu);
+    return;
   }
-  x86_adapt_put_device(type, cpu);
 }
 
 int main(int argc, char ** argv)
@@ -118,6 +94,8 @@ int main(int argc, char ** argv)
   char * knob=NULL;
   char * new_setting_str=NULL;
   int64_t new_setting=-1;
+  struct x86_adapt_configuration_item item;
+  int nr_items,ci_nr;
 
 
   while (1)
@@ -126,7 +104,7 @@ int main(int argc, char ** argv)
       {
           {"help", no_argument, 0, 'h'},
           {"hex", no_argument, 0, 'H'},
-          {"option", required_argument, 0, 'o'},
+          {"item", required_argument, 0, 'i'},
           {"value", required_argument, 0, 'V'},
           {"die", no_argument, 0, 'd'},
           {"cpu", required_argument, 0, 'c'},
@@ -135,7 +113,7 @@ int main(int argc, char ** argv)
       };
     int option_index = 0;
 
-    c = getopt_long(argc, argv, "hHc:o:V:dv", long_options, &option_index);
+    c = getopt_long(argc, argv, "hHc:i:V:nv", long_options, &option_index);
     if (c == -1)
       break;
     switch (c) {
@@ -145,13 +123,13 @@ int main(int argc, char ** argv)
     case 'H':
       hex = 1;
       break;
-    case 'd':
+    case 'n':
       type = X86_ADAPT_DIE;
       break;
     case 'c':
       cpu = atoll(optarg);
       break;
-    case 'o':
+    case 'i':
       knob=strdup(optarg);
       break;
     case 'V':
@@ -177,20 +155,48 @@ int main(int argc, char ** argv)
     exit(1);
   }
 
+  /* get item index */
+  nr_items = x86_adapt_get_number_cis(type);
+  for ( ci_nr = 0 ; ci_nr < nr_items ; ci_nr++ )
+  {
+    if (x86_adapt_get_ci_definition(type,ci_nr,&item))
+    {
+      fprintf(stderr,"Could not read item definition\n");
+      exit(1);
+    }
+    if (strcmp(item.name,knob)==0)
+    {
+	break;
+    }
+  }
+  /* not found? */
+  if ( ci_nr==nr_items )
+  {
+    fprintf(stderr,"The option %s is not available. Available options are listed now:\n",knob);
+    print_cis(type, verbose);
+    exit(1);
+  }
+
+  /* convert setting friom string to int */
   if ( hex )
     new_setting=strtoll(new_setting_str,NULL,16);
   else
     new_setting=strtoll(new_setting_str,NULL,10);
 
+  /* set setting */
   if ( cpu == -1 )
   {
     for ( cpu = 0 ; cpu < x86_adapt_get_nr_avaible_devices(type) ; cpu++ )
     {
-      set_cpu(type, cpu, knob, new_setting,verbose);
+      if  (verbose)
+        fprintf(stdout,"Set definition %s on %s %d to %"PRIi64"\n",knob,type==X86_ADAPT_DIE?"node":"CPU",cpu,new_setting);
+      set_cpu(type, cpu, ci_nr, new_setting,verbose);
     }
   } else
   {
-    set_cpu(type, cpu, knob, new_setting,verbose);
+    if  (verbose)
+      fprintf(stdout,"Set definition %s on %s %d to %"PRIi64"\n",knob,type==X86_ADAPT_DIE?"node":"CPU",cpu,new_setting);
+    set_cpu(type, cpu, ci_nr, new_setting,verbose);
   }
 
   return 0;
