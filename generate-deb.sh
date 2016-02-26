429f2c0347e6e5d0b1ad0a750be69d8ed0d99f2a
Michael Werner <xaseron@googlemail.com>
2016-02-26 16:03:49 +0100

    updated debian/ubuntu package script

--- generate-deb.sh
+++ generate-deb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# this script generates a debian/ubuntu package with dkms support
 
 #remove old directory
 if [ -d debpack ]; then
@@ -10,21 +11,35 @@ cp -r debpack_template debpack
 
 #set revision
 if [ -z "$1" ]; then
-    REVISION=`svn info | grep Revision | cut -d " " -f 2`
+    REVISION=$(git log -1 --date=short --pretty=format:%cd)
 else
     REVISION=$1
 fi
 echo $REVISION
 sed -i "s/###VERSION###/$REVISION/g" debpack/DEBIAN/*
-sed -i "s/###VERSION###/$REVISION/g" debpack/usr/src/x86_adapt_driver-template/dkms.conf
+sed -i "s/###VERSION###/$REVISION/g" debpack/usr/src/x86_adapt_defs-template/dkms.conf
+sed -i "s/###VERSION###/$REVISION/g" debpack/usr/src/x86_adapt_driver-template/*
 
+#create uncore knobs
+cd definition_driver/knobs
+./write_uncore_pmc_definitions.py
+cd -
+
+## x86_adapt_defs
+#create definitions driver source
+./definition_driver/prepare.py debpack/usr/src/x86_adapt_defs-template/src definition_driver/
+#copy header
+cp definition_driver/x86_adapt_defs.h debpack/usr/src/x86_adapt_defs-template/definition_driver/
+
+## x86_adapt_driver
 #copy files
-cp x86_adapt_driver_template.c debpack/usr/src/x86_adapt_driver-template/
-cp *.py debpack/usr/src/x86_adapt_driver-template/
-cp -r knobs debpack/usr/src/x86_adapt_driver-template/
-cp -r processors debpack/usr/src/x86_adapt_driver-template/
+cp -r driver/. debpack/usr/src/x86_adapt_driver-template/src
+# TODO: maybe change make file
+./definition_driver/prepare.py debpack/usr/src/x86_adapt_driver-template/definition_driver definition_driver/
+cp definition_driver/x86_adapt_defs.h debpack/usr/src/x86_adapt_driver-template/definition_driver/
 
 #rename dkms directory
+mv debpack/usr/src/x86_adapt_defs-template/ debpack/usr/src/x86_adapt_defs-$REVISION
 mv debpack/usr/src/x86_adapt_driver-template/ debpack/usr/src/x86_adapt_driver-$REVISION
 
 #build deb
