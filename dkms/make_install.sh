cf9183f7875328be590278c6b7bf59dae33a1979
Andreas Gocht <andreas.gocht@tu-dresden.de>
2017-11-30 21:25:54 +0100

    fix a typo in dkms make_install.sh

--- dkms/make_install.sh
+++ dkms/make_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo cp -r x86_adapt* /usr/src
-sudo dkms add -m x86_adapt_definition -v 0.1
+sudo dkms add -m x86_adapt_defs -v 0.1
 sudo dkms add -m x86_adapt_driver -v 0.3
 
 sudo dkms build -m x86_adapt_defs -v 0.1
